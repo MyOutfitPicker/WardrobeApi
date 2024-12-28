ebe819f135cac8084f0d327d1b8d5d4d52336cdc
Brandon Lee Tran <brandonleetran@icloud.com>
2024-12-27 19:08:47 -0600

    feat: initialize enum types

---
+++ Enums/Color.cs
@@ -0,0 +1,64 @@
+// -----------------------------------------------------------------------
+// <copyright file="Color.cs" company="MyWardrobe">
+//     Copyright (c) MyWardrobe. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+// <summary>
+//     Represents the color of the clothing item.
+// </summary>
+// -----------------------------------------------------------------------
+
+namespace MyWardrobeApi.Enums
+{
+    /// <summary>
+    /// Represents the color of the clothing item.
+    /// </summary>
+    public enum Color
+    {
+        /// <summary>
+        /// The color red.
+        /// </summary>
+        Red,
+
+        /// <summary>
+        /// The color yellow.
+        /// </summary>
+        Yellow,
+
+        /// <summary>
+        /// The color orange.
+        /// </summary>
+        Orange,
+
+        /// <summary>
+        /// The color green.
+        /// </summary>
+        Green,
+
+        /// <summary>
+        /// The color blue.
+        /// </summary>
+        Blue,
+
+        /// <summary>
+        /// The color purple.
+        /// </summary>
+        Purple,
+
+        /// <summary>
+        /// The color pink.
+        /// </summary>
+        Pink,
+
+        /// <summary>
+        /// The color white.
+        /// </summary>
+        White,
+
+        /// <summary>
+        /// The color black.
+        /// </summary>
+        Black,
+    }
+}
---
+++ Enums/ItemType.cs
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------
+// <copyright file="ItemType.cs" company="MyWardrobe">
+//     Copyright (c) MyWardrobe. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+// <summary>
+//     Represents the clothing item type of the clothing item.
+// </summary>
+// -----------------------------------------------------------------------
+namespace MyWardrobeApi.Enums
+{
+    /// <summary>
+    /// Represents the broad clothing category of an item.
+    /// </summary>
+    public enum ItemType
+    {
+        /// <summary>
+        /// Represents top category.
+        /// </summary>
+        Top,
+
+        /// <summary>
+        /// Represents the bottoms category.
+        /// </summary>
+        Bottom,
+
+        /// <summary>
+        /// Represents the footwear category.
+        /// </summary>
+        Footwear,
+    }
+}
